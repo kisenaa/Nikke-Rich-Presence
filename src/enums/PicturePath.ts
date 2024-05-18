ab9142ddd37a7e392e7ac14d9d88eabb2f412c8b
Johannes Daniswara Pratama <115395772+kisenaa@users.noreply.github.com>
2024-05-18 09:23:06 +0700

    feat: update

---
+++ src/enums/PicturePath.ts
@@ -0,0 +1,8 @@
+enum PicturePath {
+    defaultImage = "/nikke-default",
+    blanc = "/blancc",
+    scarlet = "/scarlet",
+    scarlet_bs = "/scarlet_bs",
+    privaty = "/privaty",
+    viper = "/viper"
+}
